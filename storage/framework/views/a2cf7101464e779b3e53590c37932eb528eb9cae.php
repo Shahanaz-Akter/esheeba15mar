97aca3f7e272d8da7e867ce82b3f015fa99d0534
Shahanaz_Akter <shahanazakterm1999@gmail.com>
2024-03-16 00:22:57 +0600

    sdfs

--- storage/framework/views/a2cf7101464e779b3e53590c37932eb528eb9cae.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('title'); ?>
-    <title>Dashboard</title>
-<?php $__env->stopSection(); ?>
-
-<?php $__env->startSection('main'); ?>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('admin.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/858192.cloudwaysapps.com/xnrkmuucrp/public_html/resources/views/admin/dashboard.blade.php ENDPATH**/ ?>
--- storage/framework/views/fd35db20b6c6c1c6f776355fd53f7080325d6d30.php
+++
@@ -1,8 +0,0 @@
-<script>
-    var language = window.navigator.userLanguage || window.navigator.language;
-    if(language=='bn'){
-        window.location = "<?php echo e(url('/bn')); ?>";
-    }else{
-        window.location = "<?php echo e(url('/en')); ?>";
-    }
-</script><?php /**PATH /home/858192.cloudwaysapps.com/xnrkmuucrp/public_html/resources/views/website/index.blade.php ENDPATH**/ ?>
